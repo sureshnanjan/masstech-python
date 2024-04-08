5226401e4a0068e80b6b72e5782d013f06b00106
sureshnanjan <sureshnanjan@outlook.com>
2024-04-09 00:20:25 +0530

    Initial Intro files added

---
+++ directory_operations.py
@@ -0,0 +1,16 @@
+import os
+
+
+def show_all_files():
+    root = os.path.join('.', 'food')
+    for directory, subdir_list, file_list in os.walk('root'):
+        print('Directory:', directory)
+        for name in subdir_list:
+            print('Subdirectory:', name)
+            for name in file_list:
+                print('File:', name)
+    print()
+
+
+if __name__ == '__main__':
+    show_all_files()
---
+++ modify_files.py
@@ -0,0 +1,14 @@
+import datetime
+import os
+
+root = os.path.join('.', 'food')
+for directory, subdir_list, file_list in os.walk(root):
+    print("Inside the loop")
+    for name in file_list:
+        print("found files")
+        source_name = os.path.join(directory, name)
+        timestamp = os.path.getmtime(source_name)
+        modified_date = str(datetime.datetime.fromtimestamp(timestamp)).replace(':', '.')
+        target_name = os.path.join(directory, f'{modified_date}_{name}')
+        print(f'Renaming: {source_name} to: {target_name}')
+        os.rename(source_name, target_name)
