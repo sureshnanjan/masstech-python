fd423e4ac9716337ccec2e820552dfeb92a0b393
sureshnanjan <sureshnanjan@outlook.com>
2024-04-18 17:24:57 +0530

    Cleanup folders and moved all to packages

--- day-01/assignments/string_operations.py
+++
@@ -1,8 +0,0 @@
-"""
-This assignment is a knowledge check to use string operations
-"""
-
-
-def make_list_from_string(input: str):
-    """This method breaks the input string and returns a list with its characters"""
-    pass
