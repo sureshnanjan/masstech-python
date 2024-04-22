c2bf0fa5c47315041c461b1d34ddb1316f423ec4
Deepanraj <deepanraj.s@mastechinfotrellis.com>
2024-04-22 17:06:20 +0530

    Day 1 assignment

--- day-01/assignments/string_operations.py
+++ day-01/assignments/string_operations.py
@@ -1,8 +1,16 @@
 """
 This assignment is a knowledge check to use string operations
 """
+import pytest
 
 
-def make_list_from_string(input: str):
+#@pytest.mark.prametrize("arg1", ["Tester"])
+def make_list_from_string(arg1):
     """This method breaks the input string and returns a list with its characters"""
-    pass
+    val_list = []
+    for char in arg1:
+        val_list.append(char)
+    print(val_list)
+    assert val_list == ['D', 'e', 'e', 'p', 'a', 'n']  # Example assertion to ensure correctness
+
+print(make_list_from_string("Deepan"))
