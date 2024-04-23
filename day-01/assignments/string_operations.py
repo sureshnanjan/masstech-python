44cefa48d0d5b2bfdf7277db915cdb2af5219493
DevsivaKM <devsiva.m@mastechdigital.com>
2024-04-23 21:17:53 +0530

    Day 01 Assignment V2

--- day-01/assignments/string_operations.py
+++ day-01/assignments/string_operations.py
@@ -5,5 +5,5 @@ This assignment is a knowledge check to use string operations
 
 def make_list_from_string(inputstr):
     """This method breaks the input string and returns a list with its characters"""
-    return list(inputstr)
+    return [*inputstr]
 
--- day01/assignments/string_operations.py
+++ day01/assignments/string_operations.py
@@ -3,6 +3,6 @@ This assignment is a knowledge check to use string operations
 """
 
 
-def make_list_from_string(input: str):
+def make_list_from_string(inputstr):
     """This method breaks the input string and returns a list with its characters"""
-    pass
+    return list(inputstr)
