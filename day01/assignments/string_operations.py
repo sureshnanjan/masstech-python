df89d0b440bf71bad50405d41bbef3a9d2d05470
SugavaneswaranBalasubramaniMIT <sugavaneswaran.balasubramani@mastechdigital.com>
2024-04-24 16:30:27 +0530

    Assignment 1

--- day01/assignments/string_operations.py
+++ day01/assignments/string_operations.py
@@ -3,6 +3,6 @@ This assignment is a knowledge check to use string operations
 """
 
 
-def make_list_from_string(input: str):
+def make_list_from_string(inputString):
     """This method breaks the input string and returns a list with its characters"""
-    pass
+    return [*inputString]
--- evaluations/test_check_assignment1.py
+++ evaluations/test_check_assignment1.py
@@ -4,4 +4,5 @@ from day01.assignments.string_operations import make_list_from_string
 def test_assignment_day01():
     # TDD
     name = "hello"
+
     assert [*name] == make_list_from_string("hello")
